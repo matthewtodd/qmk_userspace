1b656ee58ea4149065a0b2357f1c15ddcf01c94c
Matthew Todd <matthew@matthewtodd.org>
2025-11-24 21:27:17 -0500

    Try CHORDAL_HOLD for better home row mods?
    https://getreuer.info/posts/keyboards/faqs/index.html#home-row-mods-are-hard-to-use

--- keyboards/ferris/sweep/keymaps/matthewtodd/config.h
+++ keyboards/ferris/sweep/keymaps/matthewtodd/config.h
@@ -34,6 +34,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 // Pick good defaults for enabling homerow modifiers
 #define TAPPING_TERM 200
+#define CHORDAL_HOLD
 #define PERMISSIVE_HOLD
 #define QUICK_TAP_TERM 0
 
